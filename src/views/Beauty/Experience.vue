faf7dcd1ae7fc4f3cf7f934febbc7f243b91ac34
栓保 <1532164967@qq.com>
2020-12-30 08:23:32 +0800

    终极版

--- src/views/Beauty/Experience.vue
+++ src/views/Beauty/Experience.vue
@@ -1,7 +1,18 @@
 <template>
     <div>
         <!-- title -->
-        <HeaderNav />
+        <!-- <HeaderNav /> -->
+        <div style="height: 50px">
+            <div :class="{ header: show }">
+                <van-nav-bar
+                    title="本店商品"
+                    left-text="返回"
+                    left-arrow
+                    @click-left="onClickLeft"
+                    class="headerTitle"
+                />
+            </div>
+        </div>
         <van-loading size="24px" type="spinner" vertical v-show="isLoading"
             >加载中...</van-loading
         >
@@ -25,10 +36,7 @@
                 </template>
 
                 <template #footer>
-                    <van-button
-                        class="btn"
-                        size="mini"
-                        @click="gotoCard(item.id)"
+                    <van-button class="btn" size="mini" @click="gotoCard(item.id)"
                         >加入购物车</van-button
                     >
                 </template>
@@ -41,32 +49,61 @@ import url from "@/config/url";
 import Vue from "vue";
 import HeaderNav from "@/components/Header/HeaderNav";
 //列表
-import { Card, Tag, Button, Image, Loading } from "vant";
+import {
+    Toast,
+    NavBar,
+    Card,
+    Tag,
+    Button,
+    Image as VanImage,
+    Loading,
+} from "vant";
+Vue.use(NavBar);
+Vue.use(Toast);
 //项目
 Vue.use(Card);
 Vue.use(Tag);
 Vue.use(Button);
-Vue.use(Image);
+Vue.use(VanImage);
 Vue.use(Loading);
 export default {
     data() {
         return {
+            show: true,
             list: [],
             isLoading: true,
         };
     },
+
     created() {
         this.$http.get(url.getExperience).then((ret) => {
+            // console.log(ret.data.list);
             this.list = ret.data.list;
             this.isLoading = false;
         });
 
         //通知 App.vue 隐藏 底部导航
+
         // this.$store.commit("isShowFooter", false);
     },
     beforeDestroy() {
         //通知App.vue 显示底部导航
         this.$store.commit("isShowFooter", true);
+    },
+    mounted() {
+        //监听滚动条的位置
+        window.addEventListener("scroll", () => {
+            //获取滚动跳的位置
+            let top = document.documentElement.scrollTop;
+            // console.log(top);
+            if (top > 300) {
+                this.show = true;
+                // console.log(999);
+            } else {
+                this.show = false;
+                // console.log(555);
+            }
+        });
     },
 
     components: {
@@ -76,25 +113,36 @@ export default {
         onClickLeft() {
             this.$router.go(-1);
         },
+        // gotoDetail() {},
         gotoCard(cartId) {
             //存储localStorage
             this.list.forEach((el) => {
                 if (el.id == cartId) {
+                    //判断vuex 中有没有商品 没有就加 
                     if (this.$store.state.cart.length > 0) {
                         let flag = true;
+                        //vuex中有 内容 就循环
                         this.$store.state.cart.forEach((v) => {
+                            //判断是否是点击的,id匹配就弹框 切换路径
+                            //把 开关  改为 false  不执行下一步 if
                             if (v.id == cartId) {
-                                if (confirm('您的购物车中已经有该商品了，点击"确定"去购物车')) {
+                                
+                                if (
+                                    confirm(
+                                        '您的购物车中已经有该商品了，点击"确定"去购物车'
+                                    )
+                                ){
                                     this.$router.push("/order");
                                     // console.log(this.$route.path);
                                 }
                                 flag = false;
                             }
                         });
+                        // id 不匹配vuex中没有数据  然后加数据  
                         if (flag) {
                             console.log("123");
                             this.$store.commit("addCart", el);
-                            flag = false;
+                            // flag = false;
                         }
                     } else {
                         this.$store.commit("addCart", el);
@@ -110,6 +158,15 @@ export default {
 };
 </script>
 <style scoped>
+.headerTitle {
+    background-color: pink;
+}
+.header {
+    position: fixed;
+    z-index: 999;
+    width: 100%;
+    top: 0;
+}
 .card {
     margin-bottom: 50px;
 }
